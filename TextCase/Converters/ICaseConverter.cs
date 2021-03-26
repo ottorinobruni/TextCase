b7957f4880e8b45d7c4df69501e856c164d31e8c
Ottorino Bruni <ottorino.bruni@gmail.com>
2021-03-26 20:57:10 +0100

    Add Interface ICaseConverter

---
+++ TextCase/Converters/ICaseConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TextCase.Converters
+{
+    /// <summary>
+    /// Interface for Case Converter
+    /// </summary>
+    public interface ICaseConverter
+    {
+        /// <summary>
+        /// Converts a text to a string
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        string Convert(string text);
+    }
+}
