5e99e38fe35949a7967e0739d4f8f46e7b2011bd
Ottorino Bruni <ottorino.bruni@gmail.com>
2021-03-27 15:23:58 +0100

    Add TitleCaseConverter Test

---
+++ TextCase.UnitTests/TitleCaseConverterTest.cs
@@ -0,0 +1,28 @@
+using System;
+using TextCase.Converters;
+using Xunit;
+
+namespace TextCase.UnitTests
+{
+    public class TitleCaseConverterTest
+    {
+        [Theory]
+        [InlineData("hello world", "Hello World")]
+        [InlineData("icH bIn glückLICH", "Ich Bin Glücklich")]
+        [InlineData("  che ore sono? ", "  Che Ore Sono? ")]
+        [InlineData("UNICEF and children", "UNICEF And Children")]
+        public void Convert_WhenUpperCase_TextShouldBeUpperCase(string input, string output)
+        {
+            // Setup
+            var service = new TitleCaseConverter();
+
+            // Execute
+            var convertedText = service.Convert(input);
+
+            // Assert
+            var expected = output;
+            var actual = convertedText;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ TextCase/Converters/CapitaliseCaseConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace TextCase.Converters
+{
+    // <summary>
+    //// Represents a uppercase converter
+    /// </summary>
+    public class CapitaliseCaseConverter : ICaseConverter
+    {
+        /// <summary>
+        /// Converts the specified text to capitalise case.
+        /// </summary>
+        /// <param name="text">The string to convert to capitalise case.</param>
+        /// <returns>The specified text converted to capitalise case.</returns>
+        public string Convert(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            var firstPart = text.Trim().Substring(0, 1).ToUpperInvariant();
+            var secondPart = text.Trim().Length > 1 ? text.Trim()[1..].ToLowerInvariant() : "";
+            return String.Format("{0}{1}", firstPart, secondPart);
+        }
+    }
+}
