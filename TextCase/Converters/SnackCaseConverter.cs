36a9486e15f16bcfd768560964aec2b64f02a43c
Ottorino Bruni <ottorino.bruni@gmail.com>
2021-03-28 10:45:59 +0200

    Add Snack Case Converter and Test

---
+++ TextCase/Converters/SnackCaseConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using TextCase.Extensions;
+
+namespace TextCase.Converters
+{
+    // <summary>
+    //// Represents a snack converter
+    /// </summary>
+    public class SnackCaseConverter : ICaseConverter
+    {
+        /// <summary>
+        /// Converts the specified text to snack case.
+        /// </summary>
+        /// <param name="text">The string to convert to snack case.</param>
+        /// <returns>The specified text converted to snack case.</returns>
+        public string Convert(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            var result = text.ToLowerInvariant();
+            return result.Replace(" ", "_");
+        }
+    }
+}
