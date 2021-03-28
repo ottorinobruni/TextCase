f3e7e98f9cca16ac7c1c23df63f97bd1d1a7751f
Ottorino Bruni <ottorino.bruni@gmail.com>
2021-03-28 17:37:01 +0200

    Add Reverse Case and Test

---
+++ TextCase.UnitTests/ReverseCaseConverterTest.cs
@@ -0,0 +1,27 @@
+using System;
+using TextCase.Converters;
+using Xunit;
+
+namespace TextCase.UnitTests
+{
+    public class ReverseCaseConverterTest
+    {
+        [Theory]
+        [InlineData("Hello World", "dlroW olleH")]
+        [InlineData("ich bin glücklich", "hcilkcülg nib hci")]
+        [InlineData("  che ore sono? ", " ?onos ero ehc  ")]
+        public void Convert_WhenUpperCase_TextShouldBeUpperCase(string input, string output)
+        {
+            // Setup
+            var service = new ReverseCaseConverter();
+
+            // Execute
+            var convertedText = service.Convert(input);
+
+            // Assert
+            var expected = output;
+            var actual = convertedText;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
