073234884603f2a79d4317cb436e122af918d2ab
Ottorino Bruni <ottorino.bruni@gmail.com>
2021-03-28 11:43:09 +0200

    Add Hashtag Case Converter and Test

---
+++ TextCase/Converters/HashtagCaseConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using TextCase.Extensions;
+
+namespace TextCase.Converters
+{
+    // <summary>
+    //// Represents a hashtag converter
+    /// </summary>
+    public class HashtagCaseConverter : ICaseConverter
+    {
+        /// <summary>
+        /// Converts the specified text to hashtag case.
+        /// </summary>
+        /// <param name="text">The string to convert to hashtag case.</param>
+        /// <returns>The specified text converted to hashtag case.</returns>
+        public string Convert(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            var builder = new StringBuilder();
+            var words = text.Split(' ');
+            foreach (var word in words)
+            {
+                if (!String.IsNullOrEmpty(word))
+                {
+                    builder.Append($"#{word} ");
+                }
+            }
+            return builder.ToString().Trim();
+        }
+    }
+}
