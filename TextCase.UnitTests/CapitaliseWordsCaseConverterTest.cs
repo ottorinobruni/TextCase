380763330b81e889b6b0738e1679d5286c3d4584
Ottorino Bruni <ottorino.bruni@gmail.com>
2021-03-28 17:30:29 +0200

    Add CapitaliseWords Case And Test

---
+++ TextCase.UnitTests/CapitaliseWordsCaseConverterTest.cs
@@ -0,0 +1,27 @@
+using System;
+using TextCase.Converters;
+using Xunit;
+
+namespace TextCase.UnitTests
+{
+    public class CapitaliseWordsCaseConverterTest
+    {
+        [Theory]
+        [InlineData("Hello World", "Hello World")]
+        [InlineData("ICH BIN GLÜCKLICH", "Ich Bin Glücklich")]
+        [InlineData("  che ore SONO? ", "  Che Ore Sono? ")]
+        public void Convert_WhenCapitaliseCase_TextShouldBeCapitaliseCase(string input, string output)
+        {
+            // Setup
+            var service = new CapitaliseWordsCaseConverter();
+
+            // Execute
+            var convertedText = service.Convert(input);
+
+            // Assert
+            var expected = output;
+            var actual = convertedText;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
