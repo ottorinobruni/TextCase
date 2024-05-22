834964ca293d85bcde882e163dca99234364baec
Ottorino Bruni <ottorino.bruni@gmail.com>
2024-05-22 21:30:18 +0200

    Add Cobol Case

--- TextCase.UnitTests/ConstantCaseConverterTest.cs
+++ TextCase.UnitTests/ConstantCaseConverterTest.cs
@@ -1,5 +1,4 @@
 ﻿using TextCase.Converters;
-using TextCase.Converters.TextCase.Converters;
 using Xunit;
 
 namespace TextCase.UnitTests
@@ -10,6 +9,14 @@ namespace TextCase.UnitTests
         [InlineData("hello world", "HELLO_WORLD")]
         [InlineData("icH bIn glückLICH", "ICH_BIN_GLÜCKLICH")]
         [InlineData("indent_style = space, 2-\nindent_size = 2", "INDENT_STYLE_SPACE_2\nINDENT_SIZE_2")]
+        [InlineData("Hello World!", "HELLO_WORLD")]
+        [InlineData("123 test", "123_TEST")]
+        [InlineData("test with multiple   spaces", "TEST_WITH_MULTIPLE_SPACES")]
+        [InlineData("test_with_underscore", "TEST_WITH_UNDERSCORE")]
+        [InlineData("test-with-dash", "TEST_WITH_DASH")]
+        [InlineData("test.with.period", "TEST_WITH_PERIOD")]
+        [InlineData("test/with/slash", "TEST_WITH_SLASH")]
+        [InlineData("test\\with\\backslash", "TEST_WITH_BACKSLASH")]
         public void Convert_WhenConstantCase_TextShouldBeConstantCase(string input, string expected)
         {
             // Setup
--- TextCase/Converters/ConstantCaseConverter.cs
+++ TextCase/Converters/ConstantCaseConverter.cs
@@ -1,51 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace TextCase.Converters
 {
-    using System;
-    using System.Text.RegularExpressions;
-
-    namespace TextCase.Converters
+    /// <summary>
+    /// Represents a constant case converter
+    /// </summary>
+    public class ConstantCaseConverter : ICaseConverter
     {
         /// <summary>
-        /// Represents a constant case converter
+        /// Converts the specified text to constant case.
         /// </summary>
-        public class ConstantCaseConverter : ICaseConverter
+        /// <param name="text">The string to convert to constant case.</param>
+        /// <returns>The specified text converted to constant case.</returns>
+        public string Convert(string text)
         {
-            /// <summary>
-            /// Converts the specified text to constant case.
-            /// </summary>
-            /// <param name="text">The string to convert to constant case.</param>
-            /// <returns>The specified text converted to constant case.</returns>
-            public string Convert(string text)
+            if (string.IsNullOrWhiteSpace(text))
             {
-                if (string.IsNullOrWhiteSpace(text))
-                {
-                    return string.Empty;
-                }
-                // Split by newline characters to handle each line separately
-                var lines = text.Split('\n');
-
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    // Remove characters that are not letters, digits, spaces, or underscores
-                    var cleanedLine = Regex.Replace(lines[i], @"[^\w\s-]", string.Empty);
-
-                    // Replace spaces and dashes with underscores
-                    var withUnderscores = cleanedLine.Replace(' ', '_').Replace('-', '_');
-
-                    // Convert to uppercase
-                    lines[i] = withUnderscores.ToUpperInvariant();
-                }
-
-                // Join the lines back with newline characters
-                return string.Join('\n', lines);
+                return string.Empty;
             }
+
+            // Replace one or more consecutive non-alphanumeric characters (except newlines) with a single underscore
+            var replacedPunctuation = Regex.Replace(text, @"[^\w\n]+", "_");
+
+            // Split the text into lines
+            var lines = replacedPunctuation.Split('\n');
+
+            // Convert each line to upper case and trim leading and trailing underscores
+            var processedLines = lines.Select(line => line.Trim().ToUpperInvariant().Trim('_'));
+
+            // Join the lines back together
+            return string.Join("\n", processedLines);
         }
     }
-
 }
