77201b4835f20d9effefbbc2724c99e91e4d299b
Bruni, Ottorino <ottorino.bruni@zeiss.com>
2024-05-22 18:39:51 +0200

    Add Costant

---
+++ TextCase.UnitTests/ConstantCaseConverterTest.cs
@@ -0,0 +1,25 @@
+﻿using TextCase.Converters;
+using TextCase.Converters.TextCase.Converters;
+using Xunit;
+
+namespace TextCase.UnitTests
+{
+    public class ConstantCaseConverterTest
+    {
+        [Theory]
+        [InlineData("hello world", "HELLO_WORLD")]
+        [InlineData("icH bIn glückLICH", "ICH_BIN_GLÜCKLICH")]
+        [InlineData("indent_style = space, 2-\nindent_size = 2", "INDENT_STYLE_SPACE_2\nINDENT_SIZE_2")]
+        public void Convert_WhenConstantCase_TextShouldBeConstantCase(string input, string expected)
+        {
+            // Setup
+            var service = new ConstantCaseConverter();
+
+            // Execute
+            var actual = service.Convert(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ TextCase/Converters/ConstantCaseConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCase.Converters
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    namespace TextCase.Converters
+    {
+        /// <summary>
+        /// Represents a constant case converter
+        /// </summary>
+        public class ConstantCaseConverter : ICaseConverter
+        {
+            /// <summary>
+            /// Converts the specified text to constant case.
+            /// </summary>
+            /// <param name="text">The string to convert to constant case.</param>
+            /// <returns>The specified text converted to constant case.</returns>
+            public string Convert(string text)
+            {
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    return string.Empty;
+                }
+                // Split by newline characters to handle each line separately
+                var lines = text.Split('\n');
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    // Remove characters that are not letters, digits, spaces, or underscores
+                    var cleanedLine = Regex.Replace(lines[i], @"[^\w\s-]", string.Empty);
+
+                    // Replace spaces and dashes with underscores
+                    var withUnderscores = cleanedLine.Replace(' ', '_').Replace('-', '_');
+
+                    // Convert to uppercase
+                    lines[i] = withUnderscores.ToUpperInvariant();
+                }
+
+                // Join the lines back with newline characters
+                return string.Join('\n', lines);
+            }
+        }
+    }
+
+}
